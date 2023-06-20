3afbac90ddd66c0c7faaa3b8d727c37ae2c7cc54
Emre Yıldız <44emreyildiz@gmail.com>
2023-06-20 09:24:46 +0300

    Update post.cy.js

--- cypress/e2e/post.cy.js
+++ cypress/e2e/post.cy.js
@@ -8,7 +8,7 @@ describe('Post uygulaması', () => {
     });
   
     it('Yeni bir gönderi eklemek için PostForm kullanılmalı', () => {
-      cy.contains('Yeni Gönderi Ekle').click(); // "Yeni Gönderi Ekle" düğmesine tıklıyoruz
+      cy.contains('Create New Post').click(); // "Yeni Gönderi Ekle" düğmesine tıklıyoruz
       cy.get('#title-input').type('Yeni Gönderi Başlığı'); // Başlık alanına bir değer giriyoruz
       cy.get('#content-input').type('Yeni gönderi içeriği'); // İçerik alanına bir değer giriyoruz
       cy.contains('Gönder').click(); // "Gönder" düğmesine tıklıyoruz
@@ -17,10 +17,10 @@ describe('Post uygulaması', () => {
     });
   
     it('Mevcut bir gönderiyi güncellemek için UpdatePost kullanılmalı', () => {
-      cy.contains('Düzenle').first().click(); // İlk gönderinin "Düzenle" düğmesine tıklıyoruz
+      cy.contains('Update').first().click(); // İlk gönderinin "Düzenle" düğmesine tıklıyoruz
       cy.get('#title-input').clear().type('Güncellenmiş Başlık'); // Başlığı güncelliyoruz
       cy.get('#content-input').clear().type('Güncellenmiş içerik'); // İçeriği güncelliyoruz
-      cy.contains('Güncelle').click(); // "Güncelle" düğmesine tıklıyoruz
+      cy.contains('Update').click(); // "Güncelle" düğmesine tıklıyoruz
       cy.get('.success-message').should('be.visible'); // Başarılı bir mesajın görüntülendiğini kontrol ediyoruz
       cy.get('.post-list-item').should('have.length.greaterThan', 0); // Gönderi listesinin güncellendiğini kontrol ediyoruz
     });
